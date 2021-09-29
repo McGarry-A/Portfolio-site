9dfae9767bb7423b121aa0c0913f7bf6c26910ce
McGarry-A <90775883+McGarry-A@users.noreply.github.com>
2021-09-29 20:41:41 +0100

    redesign and improvements to responsiveness

---
+++ portfolio page/script.js
@@ -0,0 +1,18 @@
+
+const projectsContainer = document.getElementById('projects-container');
+const card = document.createElement('div')
+
+card.style.backgroundColor = 'red';
+
+card.classList.add('card')
+projectsContainer.appendChild('card');
+
+const cardImage = document.createElement('image')
+const cardTitle = document.createElement('h4')
+const cardP = document.createElement('p')
+
+card.appendChild('cardImage', 'cardTitle', 'cardP');
+
+
+
+
