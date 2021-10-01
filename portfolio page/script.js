30bb7e6e2859195538e51111506ef8b0f76247c2
McGarry-A <90775883+McGarry-A@users.noreply.github.com>
2021-10-01 17:05:05 +0100

    Add files via upload
    hamburger menu

--- portfolio page/script.js
+++ portfolio page/script.js
@@ -1,18 +1,8 @@
+const hamburger = document.getElementById('hamburger')
+const navUL = document.getElementById('nav-ul')
+const resumeButton = document.getElementById('resume-button')
 
-const projectsContainer = document.getElementById('projects-container');
-const card = document.createElement('div')
-
-card.style.backgroundColor = 'red';
-
-card.classList.add('card')
-projectsContainer.appendChild('card');
-
-const cardImage = document.createElement('image')
-const cardTitle = document.createElement('h4')
-const cardP = document.createElement('p')
-
-card.appendChild('cardImage', 'cardTitle', 'cardP');
-
-
-
-
+hamburger.addEventListener('click', ()=>{
+    navUL.classList.toggle('show');
+    resumeButton.style.display = 'none'
+})
